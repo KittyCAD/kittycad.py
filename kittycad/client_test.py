e9c5722b1d50b6a20bcec5b1e39743cb6bd01e3c
Jess Frazelle <github@jessfraz.com>
2021-12-15 06:42:39 -0800

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

---
+++ kittycad/client_test.py
@@ -0,0 +1,38 @@
+from ...client import AuthenticatedClientFromEnv
+from ...models import FileConversion, ValidFileTypes, AuthSession, InstanceMetadata
+from ...api.file import file_convert_with_base64_helper
+from ...api.meta import meta_debug_session, meta_debug_instance
+
+def test_get_session():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the session.
+    session: AuthSession = meta_debug_session.sync(client=client)
+
+    print(f"Session: {session}")
+
+def test_get_session_async():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the session.
+    session: AuthSession = meta_debug_session.asyncio(client=client)
+
+    print(f"Session: {session}")
+
+def test_get_instance():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the instance.
+    instance: InstanceMetadata = meta_debug_instance.sync(client=client)
+    print(f"Instance: {instance}")
+
+def test_get_instance_async():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the instance.
+    instance: InstanceMetadata = await meta_debug_instance.asyncio(client=client)
+    print(f"Instance: {instance}")
