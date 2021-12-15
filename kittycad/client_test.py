d8a9721d180b07ac2345705feb163eb616e5e442
Jess Frazelle <github@jessfraz.com>
2021-12-15 06:58:54 -0800

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/client_test.py
+++ kittycad/client_test.py
@@ -2,9 +2,9 @@ import pytest
 import asyncio
 
 from .client import AuthenticatedClientFromEnv
-from .models import FileConversion, ValidFileTypes, AuthSession, InstanceMetadata
+from .models import FileConversion, ValidFileTypes, AuthSession, InstanceMetadata, Message
 from .api.file import file_convert_with_base64_helper
-from .api.meta import meta_debug_session, meta_debug_instance
+from .api.meta import meta_debug_session, meta_debug_instance, ping
 
 def test_get_session():
     # Create our client.
@@ -51,3 +51,26 @@ async def test_get_instance_async():
     assert instance != None
 
     print(f"Instance: {instance}")
+
+def test_ping():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the message.
+    message: Message = ping.sync(client=client)
+
+    assert message != None
+
+    print(f"Message: {message}")
+
+@pytest.mark.asyncio
+async def test_ping_async():
+    # Create our client.
+    client = AuthenticatedClientFromEnv()
+
+    # Get the message.
+    message: Message = await ping.asyncio(client=client)
+
+    assert message != None
+
+    print(f"Message: {message}")
