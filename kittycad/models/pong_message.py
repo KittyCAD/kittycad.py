5b77fbdbd0d1245a38d16482ed3823c2ea329ee7
Jess Frazelle <github@jessfraz.com>
2022-02-27 20:14:39 -0800

    add autopep8
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/models/pong_message.py
+++ kittycad/models/pong_message.py
@@ -7,58 +7,56 @@ from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="PongMessage")
 
-
 @attr.s(auto_attribs=True)
 class PongMessage:
-    """ """
-    message: Union[Unset, PongEnum] = UNSET
+	""" """
+	message: Union[Unset, PongEnum] = UNSET
 
-    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+	additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
 
-    def to_dict(self) -> Dict[str, Any]:
-        message: Union[Unset, str] = UNSET
-        if not isinstance(self.message, Unset):
-            message = self.message.value
+	def to_dict(self) -> Dict[str, Any]:
+		message: Union[Unset, str] = UNSET
+		if not isinstance(self.message, Unset):
+			message = self.message.value
 
-        field_dict: Dict[str, Any] = {}
-        field_dict.update(self.additional_properties)
-        field_dict.update({})
-        if message is not UNSET:
-            field_dict['message'] = message
+		field_dict: Dict[str, Any] = {}
+		field_dict.update(self.additional_properties)
+		field_dict.update({})
+		if message is not UNSET:
+			field_dict['message'] = message
 
-        return field_dict
+		return field_dict
 
-    @classmethod
-    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
-        d = src_dict.copy()
-        _message = d.pop("message", UNSET)
-        message: Union[Unset, PongEnum]
-        if not isinstance(_message, Unset):
-            message = UNSET
-        else:
-            message = PongEnum(_message)
-
-        pong_message = cls(
-            message=message,
-        )
-
-        pong_message.additional_properties = d
+	@classmethod
+	def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+		d = src_dict.copy()
+		_message = d.pop("message", UNSET)
+		message: Union[Unset, PongEnum]
+		if not isinstance(_message, Unset):
+			message = UNSET
+		else:
+			message = PongEnum(_message)
 
 
+		pong_message = cls(
+			message= message,
+		)
+
+		pong_message.additional_properties = d
 return pong_message
 
-  @property
-   def additional_keys(self) -> List[str]:
-        return list(self.additional_properties.keys())
+	@property
+	def additional_keys(self) -> List[str]:
+		return list(self.additional_properties.keys())
 
-    def __getitem__(self, key: str) -> Any:
-        return self.additional_properties[key]
+	def __getitem__(self, key: str) -> Any:
+		return self.additional_properties[key]
 
-    def __setitem__(self, key: str, value: Any) -> None:
-        self.additional_properties[key] = value
+	def __setitem__(self, key: str, value: Any) -> None:
+		self.additional_properties[key] = value
 
-    def __delitem__(self, key: str) -> None:
-        del self.additional_properties[key]
+	def __delitem__(self, key: str) -> None:
+		del self.additional_properties[key]
 
-    def __contains__(self, key: str) -> bool:
-        return key in self.additional_properties
+	def __contains__(self, key: str) -> bool:
+		return key in self.additional_properties
