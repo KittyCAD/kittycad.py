a8d537e29cbcfb7f929fd97d7ac112efb85fc590
Jess Frazelle <github@jessfraz.com>
2022-04-06 21:14:32 -0700

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/models/api_call_query_group_by.py
+++ kittycad/models/api_call_query_group_by.py
@@ -1,12 +1,13 @@
 from enum import Enum
 
-class ApiCallQueryGroupBy(str, Enum):
-	EMAIL = 'email'
-	METHOD = 'method'
-	ENDPOINT = 'endpoint'
-	USER_ID = 'user_id'
-	ORIGIN = 'origin'
-	IP_ADDRESS = 'ip_address'
 
-	def __str__(self) -> str:
-		return str(self.value)
+class ApiCallQueryGroupBy(str, Enum):
+    EMAIL = 'email'
+    METHOD = 'method'
+    ENDPOINT = 'endpoint'
+    USER_ID = 'user_id'
+    ORIGIN = 'origin'
+    IP_ADDRESS = 'ip_address'
+
+    def __str__(self) -> str:
+        return str(self.value)
--- kittycad/models/file_conversion_source_format.py
+++ kittycad/models/file_conversion_source_format.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
-class FileConversionSourceFormat(str, Enum):
-	STL = 'stl'
-	OBJ = 'obj'
-	DAE = 'dae'
-	STEP = 'step'
-	FBX = 'fbx'
 
-	def __str__(self) -> str:
-		return str(self.value)
+class FileConversionSourceFormat(str, Enum):
+    STL = 'stl'
+    OBJ = 'obj'
+    DAE = 'dae'
+    STEP = 'step'
+    FBX = 'fbx'
+
+    def __str__(self) -> str:
+        return str(self.value)
