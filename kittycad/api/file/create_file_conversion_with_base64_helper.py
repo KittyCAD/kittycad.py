d126125770aac2d08fd1e6dfd3eff2c4c731cae8
Jess Frazelle <github@jessfraz.com>
2023-08-17 14:01:41 -0700

    fixes
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- kittycad/api/file/create_file_conversion_with_base64_helper.py
+++ kittycad/api/file/create_file_conversion_with_base64_helper.py
@@ -26,7 +26,7 @@ def sync(
 
     if isinstance(fc, FileConversion) and fc.output != "":
         if isinstance(fc.output, str):
-            b = base64.b64decode(fc.output + "===")
+            b = base64.b64decode(fc.output, validate=False)
             return (fc, b)
 
     return fc
@@ -52,7 +52,7 @@ async def asyncio(
 
     if isinstance(fc, FileConversion) and fc.output != "":
         if isinstance(fc.output, str):
-            b = base64.b64decode(fc.output + "===")
+            b = base64.b64decode(fc.output, validate=False)
             return (fc, b)
 
     return fc
